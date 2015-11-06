1261b088fbfddbee9a409f3aa33a5fc36a3a6874
Toni Hermoso Pulido <toniher@cau.cat>
2015-11-07 00:47:01 +0100

    csv

--- csv.pl
+++ csv.pl
@@ -2,23 +2,18 @@
 use strict;
 use warnings;
 use 5.010; #Version 5.10 at least
-use Data::Dumper;
 use Text::CSV;
 
 my $file = $ARGV[0] // "file.csv";
-my $csv = Text::CSV->new ( { binary => 1, sep_char => ';', empty_is_undef => 1 } )
+my $csv = Text::CSV->new ( { binary => 1, sep_char => "\t", quote_char => "", empty_is_undef => 1 } )
                  or die "Cannot use CSV: ".Text::CSV->error_diag ();
 
 open my $fh, "<:encoding(utf8)", "$file" or die "$file: $!";
 
-while (<$fh> ) {
+while ( my $row = $csv->getline( $fh ) ) {
 
-	unless ( $_=~/^\#/ ) {
-		my $status = $csv->parse($_);
-		print $_;
-		print Dumper( $csv->fields() );
+	if ( $row->[2] ) {
+		print $row->[2].": ".$row->[3]."-".$row->[4]."\n";
 	}
 }
 
-
-# Finish here
