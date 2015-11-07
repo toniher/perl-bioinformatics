80b6622cca8ac751a1e7e63eb1663ac07eb1e5d7
Toni Hermoso Pulido <toniher@cau.cat>
2015-11-07 10:11:24 +0100

    Update a bit

---
+++ bioseq.pl
@@ -0,0 +1,42 @@
+#!/usr/bin/env perl
+
+use Bio::Seq;
+use Data::Dumper;
+
+# http://search.cpan.org/dist/BioPerl/Bio/Seq.pm
+my $seqobj = Bio::Seq->new( -display_id => 'MySequence',
+	-seq => "GATACAGATACA",
+	-desc => "New sequence",
+	-species => "Homo sapiens"
+);
+
+print Dumper( $seqobj );
+
+use Bio::SeqFeature::Generic;
+
+# http://search.cpan.org/dist/BioPerl/Bio/SeqFeature/Generic.pm
+my $feat = Bio::SeqFeature::Generic->new( 
+    -start        => 10, 
+    -end          => 100,
+    -strand       => -1, 
+    -primary      => 'repeat', # -primary_tag is a synonym
+    -source_tag   => 'repeatmasker',
+    -display_name => 'alu family',
+    -score        => 1000,
+    -tag          => { new => 1,
+                       author => 'someone',
+                       sillytag => 'this is silly!' } );
+
+$seqobj->add_SeqFeature($feat);
+
+print Dumper( $seqobj );
+
+use Bio::SeqIO;
+
+my $outfasta  = Bio::SeqIO->new(-file => ">myseq.fasta" ,
+                       -format => 'Fasta');
+
+
+$outfasta->write_seq($seqobj);
+
+
--- csv.pl
+++ csv.pl
@@ -10,10 +10,28 @@ my $csv = Text::CSV->new ( { binary => 1, sep_char => "\t", quote_char => "", em
 
 open my $fh, "<:encoding(utf8)", "$file" or die "$file: $!";
 
+my @rows;
+
 while ( my $row = $csv->getline( $fh ) ) {
 
 	if ( $row->[2] ) {
 		print $row->[2].": ".$row->[3]."-".$row->[4]."\n";
+		my $list = [ $row->[2], $row->[3], $row->[4] ];
+		push( @rows, $list );
 	}
 }
 
+close $fh;
+
+my $csvout = Text::CSV->new ( { binary => 1, sep_char => ",", quote_char => '\"', eol => "\n" } )
+                 or die "Cannot use CSV: ".Text::CSV->error_diag ();
+
+open my $fhout, ">:encoding(utf8)", "out.csv" or die "out.csv: $!";
+
+while ( my $row = shift( @rows ) ) {
+	$csvout->print($fhout, $row );
+}
+
+
+
+close $fhout;
